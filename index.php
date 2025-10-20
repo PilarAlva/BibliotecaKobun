e91359aca3b1e19da3c89c4226ff0ab53f70fc31
Pilar <pilaralvarez080@gmail.com>
2025-10-20 20:49:46 -0300

    Desarrollo del header

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca Kobun - Inicio</title>
+</head>
+<body>
+    <header>
+        <include>vistas/header.php</include>
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
