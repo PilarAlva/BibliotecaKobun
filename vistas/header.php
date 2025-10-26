34a8d1225308c6d32b0bcb68e7dd6775710af8c4
Pilar <pilaralvarez080@gmail.com>
2025-10-26 11:11:36 -0300

    Desarrollo Página "Inicio"

--- vistas/header.php
+++ vistas/header.php
@@ -1,15 +1,17 @@
-<div class="header-container">
-    <a href="index.php">
-        <img src="vistas/img/logo.svg" alt="Logo" class="logo">
-    </a>
+<div class="header">
+    <div>
+        <a href="index.php">
+            <img src="vistas/img/logo.svg" alt="logo" width="150">
+        </a>      
+    </div>
 
-    <button class="hamburger-menu" aria-label="Abrir menú" aria-expanded="false">
+    <!-- <button class="hamburger-menu" aria-label="Abrir menú" aria-expanded="false">
         <span class="hamburger-box">
             <span class="hamburger-inner"></span>
         </span>
-    </button>
+    </button> -->
     
-    <nav class="main-nav" id="main-nav">
+    <nav class="main-nav">
         <ul>
             <li><a href="index.php">Inicio</a></li>
             <li><a href="#">Catálogo</a></li>
