f8dcad9eac4e250eac05f907c89f194b299d5913
FelipeDaRo5a <77176140+FelipeDaRo5a@users.noreply.github.com>
2025-10-27 21:49:35 -0300

    Agregada clase base de datos

---
+++ app/bd/BaseDatos.php
@@ -0,0 +1,61 @@
+<?php
+
+    class BaseDatos{
+        private $host = BD_HOST;
+        private $usuario = BD_USUARIO;
+        private $clave = BD_CLAVE;
+        private $bdNombre = BD_NOMBRE;
+        private $clave = BD_CLAVE;
+        private $puerto = BD_PUERTO;
+        
+        private $dbh;
+        private $stmt;
+        private $error;
+        
+        public function __construct(){
+
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->bdNombre . ';port' . $this-> puerto;
+            $options = [
+                PDO::ATTR_PERSISTENT = true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ];
+
+            try{
+                $this->dbh = new PDO($dsn, $this->usuario, $this->clave, $options);
+            }catch (PDOException $e){
+                $this->error = $e->getMessage();
+                echo $this->error;
+            }
+        }
+
+        public function consulta($sql){
+
+            $this->stmt = $this->dbh->prepare($sql);
+
+        }
+        public function ejecutar(){
+
+            return $this->stmt->execute();
+
+        }
+
+        public function resultados(){
+
+            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+        public function resultado(){
+
+                $this->ejecutar();
+                return $this->stmt->fetch(PDO::FETCH_ASSOC);
+
+        }
+        public function unir($param, $valor){
+
+            $this->stmt->bindValue($param, $valor);
+
+        }
+
+    }
+
+?>
---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+    define('BD_HOST', 'localhost');
+    define('BD_USUARIO', 'root');
+    define('BD_CLAVE', '');
+    define('BD_NOMBRE', 'kobun_db');
+    define('BD_PUERTO', '3306');
+    define('BASE_URL', 'http://localhost/kobun/BibliotecaKobun/public/');
+    
+?>
