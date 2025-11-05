4c9c9c853605425379a24661cbd7e7f5a8bfe711
FelipeDaRo5a <77176140+FelipeDaRo5a@users.noreply.github.com>
2025-11-05 11:06:20 -0300

    Tallers 0.1 terminado

---
+++ app/vistas/talleres/taller/foro.php
@@ -0,0 +1,29 @@
+<div>
+
+    <div class="taller_subir">
+    <?php
+            $alcance = 'foro';
+        
+            include '../app/vistas/talleres/componentes/subir_publicacion.php'; 
+        
+            
+        ?>
+    
+    </div>    
+    <div class="taller_contenido">
+            
+        
+        <?php
+            if(isset($publicaciones))
+            foreach($publicaciones as $indice => $publicacion){
+        
+        
+                include '../app/vistas/talleres/componentes/publicacion.php';
+
+            }
+        ?>
+
+    </div>
+        
+
+</div>
---
+++ app/vistas/talleres/taller/libreta.php
@@ -0,0 +1,40 @@
+ <div class= "subir_publicacion selected">
+        
+        <div class="sp_encabezado">
+                
+                <div class="sp_encabezado_info">
+
+                    <span class="sp_encabezado_texto">
+                        Libreta
+                    </span>
+
+                <div class="sp_btn_borrar hide">
+                    X
+                </div>
+
+                </div>
+        </div>
+        <div class="sp_cuerpo selected">
+            <textarea Rows= 1
+             autocapitalize="true"
+             id="titulo"
+            placeholder="Agrega un titulo"
+             ></textarea>
+            <div id="editor-libreta"></div>
+        </div>
+
+        <div class="sp_info ">
+
+            
+           
+            <div class="sp_btn_publicar selected">
+                <span>
+                    SUBIR
+                </span>
+            </div>
+
+        </div>
+
+         
+
+</div>
