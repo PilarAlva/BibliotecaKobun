865dcbb97906cef066b5e3eaa645fa46f48a29b5
Pilar <132228371+PilarAlva@users.noreply.github.com>
2025-11-13 18:37:01 -0300

    Update info.php

--- app/vistas/talleres/taller/info.php
+++ app/vistas/talleres/taller/info.php
@@ -18,60 +18,57 @@
         </div>
 
         <?php if ($estado == 'admin'):?>
-            <!-- VISTA DEL ADMIN DEL TITULO -->
-            <div id="titulo-editable">
-                <input type="text" name="nombre" value="<?php echo $taller["nombre"]; ?>" placeholder="Nombre del Taller">
-                <h3>Profesor/a: <?php echo $taller["profesores_nombre"]?></h3>
-            </div>
+            <!-- VISTA DEL ADMIN -->
+            <!-- FORMULARIO DE EDICIÓN PARA ADMIN -->
+
+            <form method="POST" action="<?php echo BASE_URL . 'taller/editar/' . $taller["taller_id"]?>">
+                <div id="titulo-editable">
+                    <input type="text" name="nombre" value="<?php echo $taller["nombre"]; ?>" placeholder="Nombre del Taller">
+                    <h3>Profesor/a: <?php echo $taller["profesores_nombre"]?></h3>
+                </div>
+            
+                <div id="form-contenedor">
+                    <div id="form-descripcion">    
+                        <textarea id="descripcion-textarea" name="descripcion" placeholder="Descripción" rows="3"><?php echo $taller["descripcion"]; ?></textarea>
+                    </div>
+                    <div>
+                        <div class="form-grupo">
+                            <p><span class="destacado">Horario:</span></p> 
+                            <input type="text" name="horario" value="<?php echo $taller["horario"]; ?>" placeholder="Horario">
+                        </div>
+                        <div class="form-grupo">
+                            <p><span class="destacado">Lugar:</span></p>
+                            <input type="text" name="lugar" value="<?php echo $taller["lugar"]; ?>" placeholder="Lugar">                     
+                        </div>                        
+                    </div>
+                </div>
+
+                <div class="editar-taller">
+                    <button id="bt-eliminar" class="bt destacado" type="">Eliminar Taller</button>
+
+                    <div class="form-grupo">
+                        <input type="checkbox" id="taller-activo" name="activo" value="1" <?php if ($taller["activo"]) echo 'checked'; ?>>
+                        <label for="taller-activo">Taller Activo</label>
+                    </div>
+                    <button class="bt destacado" type="submit">Guardar Cambios</button>
+                </div>
+            </form>
+
         <?php else: ?>
             <div id="titulo">
                 <h1><?php echo $taller["nombre"]?></h1>
                 <h3>Profesor/a: <?php echo $taller["profesores_nombre"]?></h3>
             </div>
-        <?php endif;?>
 
         <h3>Sobre el Taller</h3>
         <div class="informacion">
-            <?php if ($estado != 'admin'):?> 
-                <div id="descripcion">    
-                    <p><?php echo $taller["descripcion"]?></p>
-                </div>
-                <div>
-                    <p><span class="destacado">Horario:</span> <?php echo $taller["horario"]?>.</p>
-                    <p><span class="destacado">Lugar:</span> <?php echo $taller["lugar"]?>.</p>
-                </div>
-
-            <?php else: ?>
-                <!-- VISTA DEL ADMIN -->
-                 <!-- FORMULARIO NO FUNCIONAAAAAAALLLLLLL :) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  -->
-                <form method="POST" action="<?php echo BASE_URL . 'taller/editar/' . $taller["taller_id"]?>">
-                    <div id="form-contenedor">
-                        <div id="form-descripcion">    
-                            <textarea id="descripcion-textarea" name="descripcion" placeholder="Descripción" rows="3"><?php echo $taller["descripcion"]; ?></textarea>
-                        </div>
-                        <div>
-                            <div class="form-grupo">
-                                <p><span class="destacado">Horario:</span></p> 
-                                <input type="text" name="horario" value="<?php echo $taller["horario"]; ?>" placeholder="Horario">
-                            </div>
-                            <div class="form-grupo">
-                                <p><span class="destacado">Lugar:</span></p>
-                                <input type="text" name="lugar" value="<?php echo $taller["lugar"]; ?>" placeholder="Lugar">                     
-                            </div>                        
-                        </div>
-                    </div>
-
-                    <div class="editar-taller">
-                        <button id="bt-eliminar" class="bt destacado" type="">Eliminar Taller</button>
-
-                        <div class="form-grupo">
-                            <input type="checkbox" id="taller-activo" name="activo" value="1" <?php if ($taller["activo"]) echo 'checked'; ?>>
-                            <label for="taller-activo">Taller Activo</label>
-                        </div>
-                        <button class="bt destacado" type="submit">Guardar Cambios</button>
-                    </div>
-                </form>
-            <?php endif;?>
+            <div id="descripcion">    
+                <p><?php echo $taller["descripcion"]?></p>
+            </div>
+            <div>
+                <p><span class="destacado">Horario:</span> <?php echo $taller["horario"]?>.</p>
+                <p><span class="destacado">Lugar:</span> <?php echo $taller["lugar"]?>.</p>
+            </div>
         </div>
         
         <div class="contenedor-boton">
@@ -103,6 +100,7 @@
             <?php } ?>
         </div>
 
+    <?php endif; ?>
     </div>
 </main>
 
