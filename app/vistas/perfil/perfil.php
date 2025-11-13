4114ad8c7ca7335531e1a3f586d2eb82e209e46f
Pilar <132228371+PilarAlva@users.noreply.github.com>
2025-11-12 23:45:45 -0300

    arreglo de la vista sliders en Perfil

--- app/vistas/perfil/perfil.php
+++ app/vistas/perfil/perfil.php
@@ -4,7 +4,9 @@
     ?>
 </header>
 
+<div class="page-body"> <!-- Contenedor añadido -->
     <main class="main-content  <?php if ($usuario['rol_id'] == 1) echo "admin"; ?>">
+        
         
         <div class="perfil-contenedor">
             <div class="encabezado">
@@ -34,72 +36,63 @@
                 </nav>
 
             <!-- PESTAÑAS DEL PERFIL -->
-                <div class="contenido-perfil">
-                
-                            
-                    <?php include '../app/vistas/perfil/tabs/infoPersonal.php' ?>
+                <div class="contenido-perfil-wrapper"> <!-- Contenedor wrapper -->
+                    <div class="contenido-perfil">
+                        <!-- Todos los includes van aquí dentro -->
+                        <?php include '../app/vistas/perfil/tabs/infoPersonal.php' ?>
+                        <?php include '../app/vistas/perfil/tabs/prestamos.php' ?>
+                        <?php include '../app/vistas/perfil/tabs/talleres.php' ?>
+                        <?php include '../app/vistas/perfil/tabs/accesibilidad.php' ?>
+                        
+                        <!-- VISTAS ADICIONALES DE ADMINISTRADOR -->
+                        <?php
+                        if ($usuario['rol_id'] == 1) { ?>
+                        <?php include '../app/vistas/perfil/tabs/gestionUsuarios.php' ?>
+                        <?php include '../app/vistas/perfil/tabs/gestionMaterial.php' ?>
+                        <?php include '../app/vistas/perfil/tabs/gestionTalleres.php' ?>
+                        <?php } ?>
 
-                    <?php include '../app/vistas/perfil/tabs/prestamos.php' ?>
-
-                    <?php include '../app/vistas/perfil/tabs/talleres.php' ?>
-
-                    <?php include '../app/vistas/perfil/tabs/accesibilidad.php' ?>
-                    
-                    <!-- VISTAS ADICIONALES DE ADMINISTRADOR -->
-                    <?php
-                    if ($usuario['rol_id'] == 1) { ?>
-                    
-                    <?php include '../app/vistas/perfil/tabs/gestionUsuarios.php' ?>
-
-                    <?php include '../app/vistas/perfil/tabs/gestionMaterial.php' ?>
-
-                    <?php include '../app/vistas/perfil/tabs/gestionTalleres.php' ?>
-
-                    <?php } ?>
-
+                    </div>
                 </div>
 
             </div>
         </div>
-        
-        <!-- FORMULARIO DEL ADMIN -->
-        <?php
-    if ($usuario['rol_id'] == 1) { ?>
-        <div id= "formulario" class="form_contenedor form_oculto">
-            <div id="btn-despliege" class="form_seccion form_lengueta">
-                  <
-            </div>
-            <div class="form_cuerpo">
-                <section class="form_cabezera">
-                        <div class="form_fila rellena">
-                            <!-- <div id="btn-retroceso" class="form_boton"><</div> -->
-                            <!-- <div id="cant-cambios">Cantidad de cambios:</div> -->
-                            <!-- <div id="btn-cerrar" class="form_boton">X</div> -->
-                        </div>
+    </main>
+   
+    <!-- FORMULARIO DEL ADMIN (movido fuera del main) -->
+    <?php if ($usuario['rol_id'] == 1) { ?>
+    <div id= "formulario" class="form_contenedor form_oculto">
+        <div id="btn-despliege" class="form_seccion form_lengueta">
+                <
+        </div>
+        <div class="form_cuerpo">
+            <section class="form_cabezera">
+                    <div class="form_fila rellena">
+                        <!-- <div id="btn-retroceso" class="form_boton"><</div> -->
+                        <!-- <div id="cant-cambios">Cantidad de cambios:</div> -->
+                        <!-- <div id="btn-cerrar" class="form_boton">X</div> -->
+                    </div>
+            </section>
 
-                </section>
-
+            <div class="form-contenido-wrapper"> <!-- Contenedor wrapper -->
                 <div class="form_contenido"> 
                     Primero deberías tocar alguna opcion crack!   
                 </div>
-                    
-                    
-                <section class="form_pie">
-                    <div class="form_fila rellena">
-                        <!-- <div id= "btn-cerrar" class="form_boton rojo">Cerrar</div> -->
-                        <!-- <div id= "btn-guardar" class="form_boton verde">Guardar</div> -->
-                    </div>
-                </section>
-
             </div>
+                
+                
+            <section class="form_pie">
+                <div class="form_fila rellena">
+                    <!-- <div id= "btn-cerrar" class="form_boton rojo">Cerrar</div> -->
+                    <!-- <div id= "btn-guardar" class="form_boton verde">Guardar</div> -->
+                </div>
+            </section>
+
         </div>
-
-
+    </div>
     <?php } ?>
-        
-    </main>
-   
-    
+
+</div> <!-- Fin del contenedor page-body -->
 
 
 <footer>
