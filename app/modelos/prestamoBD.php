8683d73e3e62ea36c688c81e6c317ab2bfa36712
FelipeDaRo5a <77176140+FelipeDaRo5a@users.noreply.github.com>
2025-11-14 03:16:23 -0300

    Ya se ven bien los préstamos en perfil

--- app/modelos/prestamoBD.php
+++ app/modelos/prestamoBD.php
@@ -54,6 +54,7 @@ class prestamoBD {
     public function prestamosPorSocio($socio_id) {
         $consulta = "SELECT p.id,
                             l.titulo,
+                            l.ref_portada as portada,
                             group_concat(distinct concat(ar.nombre, ' ', ar.apellido) separator ', ') as autores,
                             p.ejemplar_id, 
                             DATE(p.fecha_prestamo) as fecha_prestamo,
--- app/vistas/perfil/tabs/prestamos.php
+++ app/vistas/perfil/tabs/prestamos.php
@@ -14,15 +14,15 @@
                                 <div class="libro">
                                     <div class="imagen-libro-cont">
                                         <?php
-                                            $portadaSrc = !empty($libros_prestados[$prestamo["ejemplar_id"]]['portada']) ? 'img/portadas/' . htmlspecialchars($libros_prestados[$prestamo["ejemplar_id"]]['portada']) : 'img/no.png';
+                                            $portadaSrc = BASE_IMG . $prestamo['portada'];;
                                         ?>
-                                        <img src="<?php echo $portadaSrc; ?>" alt="Portada del libro <?php echo htmlspecialchars($libros_prestados[$prestamo["ejemplar_id"]]['titulo']); ?>">
+                                        <img src="<?php echo $portadaSrc; ?>" alt="Portada del libro <?php echo htmlspecialchars($prestamo['titulo']); ?>">
                                     </div>
                                     <div class="info-libro-contenedor">
                                         <div>
-                                            <a href="<?=BASE_URL?>libro/id/<?= $libros_prestados[$prestamo["ejemplar_id"]]['id']; ?>">
-                                                <h4 class="titulo-libro"><?php echo htmlspecialchars($libros_prestados[$prestamo["ejemplar_id"]]['titulo']); ?></h4>
-                                                <p class="autor-libro">Por <?php echo htmlspecialchars($libros_prestados[$prestamo["ejemplar_id"]]['autores']); ?></p>
+                                            <a href="<?=BASE_URL?>libro/id/<?= $prestamo['id']; ?>">
+                                                <h4 class="titulo-libro"><?php echo htmlspecialchars($prestamo['titulo']); ?></h4>
+                                                <p class="autor-libro">Por <?php echo htmlspecialchars($prestamo['autores']); ?></p>
                                             </a>
                                         </div>
                                         <div>
@@ -30,7 +30,7 @@
                                                 <p class="devuelto">Devuelto el: <?php echo date("d/m/Y", strtotime($prestamo['fecha_devolucion'])); ?></p>
                                             <?php else: ?>
                                                 <?php if ($prestamo['fecha_vencimiento'] < date('Y-m-d')): ?>
-                                                    <p class="msj-rojo">Reservado hasta: <?php echo date("d/m/Y", strtotime($libros_prestados[$prestamo["ejemplar_id"]]['fecha_vencimiento'])); ?></p>
+                                                    <p class="msj-rojo">Reservado hasta: <?php echo date("d/m/Y", strtotime($prestamo['fecha_vencimiento'])); ?></p>
                                                 <?php else: ?>
                                                     <p>Reservado hasta: <?php echo date("d/m/Y", strtotime($prestamo['fecha_vencimiento'])); ?></p>                                                
                                                 <?php endif; ?>
