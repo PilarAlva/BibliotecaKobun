092f0012e2e5a353081bd2ed7df1ad9ad7846593
Pilar <132228371+PilarAlva@users.noreply.github.com>
2025-11-16 18:26:31 -0300

    Detalles CSS parte 542421241

--- app/vistas/talleres/componentes/subir_publicacion.php
+++ app/vistas/talleres/componentes/subir_publicacion.php
@@ -26,7 +26,7 @@
             <div class="sp_encabezado_info">
 
                 <span class="sp_encabezado_texto">
-                    Escribe aqui...
+                    Nueva Publicación
                 </span>
                 <span class="sp_encabezado_texto_usuario hide">
 
